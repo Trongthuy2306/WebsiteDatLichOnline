b9854d4fdeaca0c2bb47974ce38e5da6f437dfc1
ICT <ICT@DESKTOP-GCADVBQ>
2023-03-22 12:03:08 +0700

    Add project files.

---
+++ DocTorOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DocTorOnline.Startup))]
+namespace DocTorOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
